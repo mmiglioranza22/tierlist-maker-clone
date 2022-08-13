0670c34ae90e0f1921229ee903fe92fdcecd9246
Manuel Miglioranza <manuel.miglioranza@zenvia.com>
2022-08-12 22:41:50 -0300

    wip
    require module not working

---
+++ data-structure/matrix.js
@@ -0,0 +1,8 @@
+class Matrix {
+   constructor(){
+       this.name = 'tierliest'
+
+   } 
+    
+}
+export default Matrix
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const m = require('./data-structure/matrix') 
 console.log(window);
+const matrix = new m 
+console.log(
+    matrix.name
+);
