39609287b67e1cc7297491cd64f6624b5c0ff262
Manuel Miglioranza <manuel.miglioranza@zenvia.com>
2022-08-14 01:20:22 -0300

    wip. check prepend logic for different scenarios

--- modules/DoublyLinkedList.js
+++ modules/DoublyLinkedList.js
@@ -3,22 +3,32 @@ class DoublyLinkedList {
 	constructor(name){
 		this.name = `${name}-tier`
 		this.head = null
-		this.tail = this.head // * this solves prepending when nodes > 0
+		this.tail = this.head // * this works only when length === 0
 		this._length = 0
 	} 
 
 	prepend(value) {
 		const node = new Node(value)
 		let pointer = this.head
+		
 		if (pointer === null) {
 			this.head = node
+			this._length++
+			return this
 		}
+		
 		node.next = pointer
 		pointer.previous = node
 		this.head = node // ? does this.tail point the new node ?
 
+		while (pointer.next !== null) {
+			pointer = pointer.next
+		}
+		
+		this.tail = pointer
+
 		this._length++
-		return
+		return this
 	}
 
 	append(value){
