15421474ce8f3b98335849dd8521d8676ea362de
Manuel Miglioranza <manuel.miglioranza@zenvia.com>
2022-08-18 22:41:31 -0300

    unit tests skeleton set

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,59 @@
 const assert = require('chai').assert
 
+const Node = require('../modules/Node')
+const DLL = require('../modules/DoublyLinkedList')
 
 // const assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+describe('Test suite:', function () {
+  describe('- Node', function () {
+    it('it should create a node with data', function () {
+      
     });
+  });
+  describe('- Doubly linked list', function () {
+    it('it should create a linked list from scratch with a tier name', function () {
+      
+    });
+    it('it should prepend nodes to the head of the list', function () {
+      
+    });
+
+    it('it should append nodes to the tail of the list', function () {
+      
+    });
+
+    it('it should insert nodes in any slot/index of the list', function () {
+      
+    });
+    
+    it('it should prepend, append and insert nodes seamlessly ', function () {
+      
+    });
+
+    it('it should pop nodes from the tail of the list and return them', function () {
+      
+    });
+
+    it('it should shift nodes from the head of the list and return them', function () {
+      
+    });
+
+    it('it should remove nodes from any slot/index of the list and return them', function () {
+      
+    });
+
+    it('it should pop, shift and remove nodes from any slot/index of the list seamlessly', function () {
+      
+    });
+
+    it('impossible operations should be omitted and log correct error', function () {
+      
+    });
+
+    it('different combinations of operations should work seamlessly', function () {
+      
+    });
+
+
   });
 });
