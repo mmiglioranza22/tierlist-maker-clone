8555496e8803de0f5ffee9d24923b702338edbc3
Manuel Miglioranza <manuel.miglioranza@zenvia.com>
2022-08-27 18:05:36 -0300

    styles modified. minimal logic for drop & drag WIP

--- index.js
+++ index.js
@@ -15,13 +15,88 @@ console.log(new DoublyLinkedList('s'));
  *   (sample)
  */
 
-const node_a = document.createElement('div')
+const dropArea = document.getElementById('drop-area');
+
+dropArea.addEventListener('dragover', (event) => {
+  console.log('drag over');
+  event.stopPropagation();
+  event.preventDefault();
+  // Style the drag-and-drop as a "copy file" operation.
+  event.dataTransfer.dropEffect = 'move';
+});
+
+dropArea.addEventListener('drop', (event) => {
+  event.stopPropagation();
+  event.preventDefault();
+  // const fileList = event.dataTransfer.files;
+  console.log(event.dataTransfer);
+});
+
+const node_a = document.createElement('span')
 node_a.setAttribute('draggable', true)
 const content_a = document.createTextNode('epa')
 node_a.appendChild(content_a)
 document.querySelector('.container-options').appendChild(node_a)
 
-const img = document.createElement('img')
-img.setAttribute('src', './public/img/naruto.jpeg')
-document.querySelector('.container-options').appendChild(img)
+const img1 = document.createElement('img')
+const img2 = document.createElement('img')
+const img3 = document.createElement('img')
+
+img1.setAttribute('src', './public/img/naruto.jpeg')
+img2.setAttribute('src', './public/img/sasuke.webp')
+img3.setAttribute('src', './public/img/sakura.jpeg')
+
+img1.setAttribute('draggable', true)
+img2.setAttribute('draggable', true)
+img3.setAttribute('draggable', true)
+
+
+img1.setAttribute('height', '100px')
+img1.setAttribute('width', '100px')
+img2.setAttribute('height', '100px')
+img2.setAttribute('width', '100px')
+img3.setAttribute('height', '100px')
+img3.setAttribute('width', '100px')
+
+img1.classList.add("container-options");
+img2.classList.add("container-options");
+img3.classList.add("container-options");
+
+document.querySelector('.container-options').appendChild(img1)
+document.querySelector('.container-options').appendChild(img2)
+document.querySelector('.container-options').appendChild(img3)
+
+
+function handleDragStart(e) {
+  console.log('drag start');
+  
+  this.style.opacity = '0.4';
+
+  dragSrcEl = this;
+
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
+}
+
+function handleDragEnd(e) {
+  console.log('drag end');
+  this.style.opacity = '1';
+}
+
+function handleDrop(e) {
+  console.log('drop event');
+  e.stopPropagation(); // stops the browser from redirecting.
+  if (dragSrcEl !== this) {
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+  }
+
+  return false;
+}
+
+
+
+node_a.addEventListener('dragstart', handleDragStart);
+node_a.addEventListener('dragend', handleDragEnd);
+node_a.addEventListener('drop', handleDrop);
 
