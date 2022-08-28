859149eea6a829649454c437604eac9928a9ffd3
Manuel Miglioranza <manuel.miglioranza@zenvia.com>
2022-08-28 13:50:17 -0300

    styles in background modified

--- handlers/index.js
+++ handlers/index.js
@@ -2,19 +2,22 @@
 
 let dragged = null;
 
-export function handleDragStart(event) {
+export function handleDragStart(event, list) {
   // store a ref. on the dragged elem
   dragged = event.target
+  console.log(event.target.alt);
+
 }
 
-export function handleDragOver(event) {
+export function handleDragOver(event, list) {
    // prevent default to allow drop
    event.preventDefault()
 }
 
-export function handleDrop(event) {
+export function handleDrop(event, list) {
   // prevent default action (open as link for some elements)
   event.preventDefault()
+  console.log(event.target);
   // move dragged element to the selected drop target
   if (event.target.className === "container-tierlist") {
     dragged.parentNode.removeChild(dragged)
