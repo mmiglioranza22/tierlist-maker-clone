2f1381ab430e9e24c78387011887fb8f16e04a11
Manuel Miglioranza <manuel.miglioranza@zenvia.com>
2022-09-06 00:42:34 -0300

    wip

--- handlers/index.js
+++ handlers/index.js
@@ -2,6 +2,7 @@
 import { tiers, scrollContent } from '../init.js'
 let dragged
 let draggedParent
+let belowDragged
 
 export function handleDragEnter(event) {
   event.preventDefault()
@@ -16,7 +17,6 @@ export function handleDragEnter(event) {
     })
   }
 }
-
 
 export function handleDragStart(event) {
   // store a ref. on the dragged elem
@@ -50,35 +50,72 @@ export function handleDragStart(event) {
 export function handleDragOver(event) {
   // prevent default to allow drop
   event.preventDefault()
-  
-
+  // store ref to the element below dragged
+  if (event.target !== dragged && event.target.tagName === 'IMG') {
+    belowDragged = event.target
+    // console.log({dragged, belowDragged})
+  }
 }
 
 export function handleDrop(event) {
   // prevent default action (open as link for some elements)
   event.preventDefault()
+  console.log('onDrop -> ')
   try {
     const list = document.getElementById(event.target.id)
     let dropped = false
-  
+    const isContainer = event.target.className === "container-tierlist"
+    
+    const p = document.createElement('p')
+    const span = document.createElement('span')
     // move dragged element to the selected drop target
-    if (event.target.className === "container-tierlist") {
-      dragged.parentNode.removeChild(dragged)
-      event.target.appendChild(dragged)
-      dropped = true
-      const p = document.createElement('p')
-      const span = document.createElement('span')
-      const tierName = document.createElement('span')
+    if (isContainer) {
+      console.log('isContainer!!', {belowDragged})
+      if (belowDragged) {
 
-     
-      
+        const nodeNames = Array.from(event.target.childNodes).map(el => el.name)
+        // eslint-disable-next-line no-console
+        console.log(nodeNames)
+        const isOverLastChild = event.target.lastChild.name === belowDragged.name
+        if (isOverLastChild) {
+          // eslint-disable-next-line no-console
+          console.log(event.target.lastChild.name === belowDragged.name)
+          if (event.target.children?.length !== 0) {
+            console.log('OVER --> appends to last child', event.target.children.length  )
+            dragged.parentNode.removeChild(dragged)
+            event.target.appendChild(dragged)
+          }
+          if (event.target.children.length === 0) {
+            // eslint-disable-next-line no-console
+            console.log('OVER --> no childs in list, appends', event.target.children.length  )
+            dragged.parentNode.removeChild(dragged)
+            event.target.appendChild(dragged)
+          }
+        }
+      } else {
+        if (event.target.children?.length !== 0) {
+          console.log('children! appends to last child', event.target.children.length  )
+          dragged.parentNode.removeChild(dragged)
+          event.target.appendChild(dragged)
+        }
+        if (event.target.children.length === 0) {
+          // eslint-disable-next-line no-console
+          console.log('no childs in list, appends', event.target.children.length  )
+          dragged.parentNode.removeChild(dragged)
+          event.target.appendChild(dragged)
+        }
+      }
+    }
+   
+      dropped = true
+
       // append node to list
       // TODO: check for HTML api to insert images in between, different flow
       // * check inside tier -> 
       // * if tier hasNode(dragged.name)
       // * const node = search(dragged.name)
       // *  if node is in the middle of others (node.next !== null) or something alike
-
+    if (list) {
       tiers.forEach(tier => {
         if (dropped && tier[0].name.includes(list.id)) {
           console.log(`%c${tier[0].name} selected!`, `background: #111113; color: ${tier[1]}`)
@@ -94,18 +131,11 @@ export function handleDrop(event) {
 
         tier[0].append(dragged.name) // ? or id ?
         console.log(`${tier[0].name}.append(${dragged.name}) invoked!`)
-
-          //todo: logic to create this: 
-        // S-tier: N1 
-        // S-tier: N1 <=> N2
-
+        // logic for scroll-container (resume operations)
         span.setAttribute('id', `${tier[0].name}-span`)
         span.innerText = `${tier[0].name}: ${tier[0].printList().join(' <=> ')}`
-      
         p.appendChild(span)
-
         scrollContent.appendChild(p)
-
         }
       })
     }
