188e2cbdac091e943d69731e6f3c296783293477
Manuel Miglioranza <manuel.miglioranza@zenvia.com>
2022-09-08 22:14:19 -0300

    Refactor + changes. Insert logic now works with remove logic.
    Finally, the work is done. There are still same edge-cases for when the nodes are dropped in the same tier in edges
    or places where no drop zone exists. Yet I think a no-compromise solution is still capable (otherwise, prepare disclaimer).
    
    Basically all addition and substraction of nodes is done in the drop handler. This allows for much better control
    over the data structures and is more immediate for detectig changes, and avoids the previous logic to be handled
    in the dragend event handler (which surprisingly is called AFTER the drop event. Note to look deeper into this).
    
    All hard work seems to be done now, small modifications, utils for DRY and better stylings are pending.

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -19,30 +19,6 @@ export function handleDragStart(event) {
 
 export function handleDragEnd(event) {
     event.preventDefault()
-    // check if the container the node is dragged from is a tierlist
-    // todo add logic if dropped over img? dont think so 
-    if (draggedParent.className === "container-tierlist") {
-      tiers.forEach((tier, i) => {
-        // identify the list the node is dragged from
-        if (tier[0].name.includes(draggedParent.id)) {
-            // todo check for certain type errors, add && checks
-          if (tier[0].tail && tier[0].tail.data === dragged.name) {
-            tier[0].pop()
-            console.log(`${tier[0].name}.pop()`)
-          } else if (tier[0].head.data === dragged.name) {
-            tier[0].shift()
-            console.log(`${tier[0].name}.shift()`)
-          } else {
-            // eslint-disable-next-line no-console
-            //console.log(tier[0])
-            //tier[0].remove(dragged.name) // ! error comes here, tier[0] does not have the element to be removed
-            // const index = tier[0].getIndex(dragged.name)
-            // tier[0].removeByIndex(index)
-            console.log(`${tier[0].name}.remove(${dragged.name})`)
-          }
-        }
-      })
-    }
 }
 
 export function handleDragOver(event) {
@@ -55,63 +31,88 @@ export function handleDragOver(event) {
 }
 
 export function handleDrop(event) {
-  // prevent default action (open as link for some elements)
+  // prevent default action (open as link for some elements) // todo should use stopProp or other for erro cases?
   event.preventDefault()
+
   try {
-    const list = document.getElementById(event.target.id) // ! identify the container tier id when drop over IMG
-    
+    let list = document.getElementById(event.target.id)
     const p = document.createElement('p')
     const span = document.createElement('span')
+
     // flow control: move dragged element to the selected drop target
     // append at the end of the list if dropped in drop area
-    if (event.target.className === "container-tierlist") {
+    if (event.target.className === 'container-tierlist') {
       operation = 'append'
       dragged.parentNode.removeChild(dragged)
       event.target.appendChild(dragged)
     }
+
     // insert before last hovered node
     if (event.target.tagName === 'IMG') {
-      previousNode = belowDragged.previousSibling?.name
+      list = belowDragged.parentNode
+      previousNode = belowDragged.name
       operation = belowDragged.previousSibling ? 'insert' : 'prepend'
       dragged.parentNode.removeChild(dragged)
       event.target.parentNode.insertBefore(dragged, event.target)
     }
    
-    // append node to list for data structure operations console
-// eslint-disable-next-line no-console
-console.log({operation, list: list.id})
-      // tiers.forEach(tier => {
-      //   if (tier[0].name.includes(list.id)) {
-      //     console.log(`%c${tier[0].name} selected!`, `background: #111113; color: ${tier[1]}`)
-      //   switch (operation) {
-      //     case 'append':
-      //       tier[0].append(dragged.name)
-      //       console.log(`${tier[0].name}.append(${dragged.name})`)
-      //       break
-      //     case 'prepend':
-      //       tier[0].prepend(dragged.name) 
-      //       console.log(`${tier[0].name}.prepend(${dragged.name})`)
-      //       break
-      //     case 'insert':
-      //       const index = tier[0].getIndex(previousNode)
-      //       tier[0].insert(index ,dragged.name) 
-      //       console.log(`${tier[0].name}.insert(${index}, ${dragged.name})`)
-      //       break
-      //     default:
-      //       console.error('Unexpected operation type invoked', operation)
-      //   }
+    tiers.forEach(tier => {
+      // TODO: check order of remove/add nodes process, try SAME LIST/TIER edge cases to force errors and evaluate
+      // * strategies: fail first, condition to error free, etc
+      // * the goal is to avoid popping nodes if they are not being added 
+      
+      // store reference of list that loses a node
+      let poppedTier
+      if (tier[0]._length && tier[0].hasNode(dragged.name)) {
+        poppedTier =  tier[0]
+        if(poppedTier) {
+          // removing nodes
+            if (poppedTier.tail && poppedTier.tail.data === dragged.name) {
+            tier[0].pop()
+            console.log(`%c${tier[0].name}.pop()`, `background: #111113; color: ${tier[1]}`)
+          } else if (poppedTier.head && poppedTier.head.data === dragged.name) {
+            tier[0].shift()
+            console.log(`%c${tier[0].name}.shift()`, `background: #111113; color: ${tier[1]}`)
+          } else {
+            // poppedTier.remove(dragged.name) // could be used just fine
+            const index = poppedTier.getIndex(dragged.name)
+            poppedTier.removeByIndex(index)
+            console.log(`%c${poppedTier.name}.removeByIndex(${index}, ${dragged.name})`, `background: #111113; color: ${tier[1]}`)
+          }
+        }
+      }
 
-      //   // logic for scroll-container (resume operations)
-      //   span.setAttribute('id', `${tier[0].name}-span`)
-      //   span.innerText = `${tier[0].name}: ${tier[0].printList().join(' <=> ')}`
-      //   p.appendChild(span)
-      //   scrollContent.appendChild(p)
-      //   }
-      // })
+      // adding nodes
+      if (tier[0].name.includes(list.id)) {
+        switch (operation) {
+          case 'append':
+            tier[0].append(dragged.name)
+            console.log(`%c${tier[0].name}.append(${dragged.name})`, `background: #111113; color: ${tier[1]}`)
+            break
+          case 'prepend':
+            tier[0].prepend(dragged.name) 
+            console.log(`%c${tier[0].name}.prepend(${dragged.name})`, `background: #111113; color: ${tier[1]}`)
+            break
+          case 'insert':
+            const index = tier[0].getIndex(previousNode)
+            tier[0].insert(index ,dragged.name) 
+            console.log(`%c${tier[0].name}.insert(${index}, ${dragged.name})`, `background: #111113; color: ${tier[1]}`)
+            break
+          default:
+            console.error('Unexpected operation type invoked', operation)
+        }
+        // append node to list for data structure operations console // todo convert to util fn
+        span.setAttribute('id', `${tier[0].name}-span`)
+        span.innerText = `${tier[0].name}: ${tier[0].printList().join(' <=> ')}`
+        p.appendChild(span)
+        scrollContent.appendChild(p)
+      }
+    })
 
-    // reset belowDragged to avoid conflicts on new hover over images
+    // reset belowDragged to avoid conflicts on new hover over images // todo check this
     belowDragged = undefined
     previousNode = undefined
+    list = undefined
   } catch (error) {
     console.error('Element dropped outside drop area', error)
     return
